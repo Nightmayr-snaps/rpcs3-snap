7bf194a20c7d9e7d5aa69ac0da79f62358930916
Nightmayr <umayr23@gmail.com>
2020-10-23 23:10:23 +0100

    workflow deploying to edge channel

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -2,7 +2,7 @@
 
 # check latest released tagged version
 LATEST_VERSION_TAG="$(curl https://api.github.com/repos/RPCS3/rpcs3-binaries-linux/releases/latest -s | jq .tag_name -r)"
-CURRENT_VERSION_SNAP="$(snap info rpcs3-emu | grep stable | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+CURRENT_VERSION_SNAP="$(snap info rpcs3-emu | grep edge | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
 LATEST_VERSION_COMMIT=${LATEST_VERSION_TAG#build-}
 LATEST_VERSION="$(curl https://api.github.com/repos/RPCS3/rpcs3-binaries-linux/releases/latest -s | jq .name -r)"
 
