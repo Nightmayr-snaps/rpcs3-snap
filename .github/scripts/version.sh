963c16410f5494dc0e912df23f3c272bb56e1ce0
Nightmayr <umayr23@gmail.com>
2020-10-23 18:42:50 +0100

    changed versioning method, using repo vulkan

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# check latest tagged version
-LATEST_VERSION_TAG="$(curl https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest -s | jq .tag_name -r)"
-CURRENT_VERSION_SNAP="$(grep version snap/snapcraft.yaml | cut -d ' ' -f2 | tr -d \"\'\")"
-CURRENT_VERSION_TAG_SNAP=mainline-$CURRENT_VERSION_SNAP
-LATEST_VERSION=${LATEST_VERSION_TAG#mainline-}
+# check latest released tagged version
+LATEST_VERSION_TAG="$(curl https://api.github.com/repos/RPCS3/rpcs3-binaries-linux/releases/latest -s | jq .tag_name -r)"
+CURRENT_VERSION_SNAP="$(snap info rpcs3-emu | grep stable | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+LATEST_VERSION_COMMIT=${LATEST_VERSION_TAG#build-}
+LATEST_VERSION="$(curl https://api.github.com/repos/RPCS3/rpcs3-binaries-linux/releases/latest -s | jq .name -r)"
 
 
 # compare versions
-if [ $CURRENT_VERSION_TAG_SNAP != $LATEST_VERSION_TAG ]; then
+if [ $CURRENT_VERSION_SNAP != $LATEST_VERSION ]; then
     echo "versions don't match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
+    echo "updating rpcs3 source commit"
+    yq w -i snap/snapcraft.yaml parts.rpcs3.source-commit $LATEST_VERSION_COMMIT
     echo true >> build
     echo $LATEST_VERSION >> latest
+    echo $LATEST_VERSION_COMMIT >> latest_commit
     echo $CURRENT_VERSION_SNAP >> current
 else
-    echo "versions match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
+    echo "versions match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
     echo false >> build
 fi
 
